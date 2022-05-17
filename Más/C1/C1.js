e5480b49aea705ef2cd989a838e1bbade01822e4
zxxej <zxejimenez@gmail.com>
2022-05-17 16:45:09 -0300

    Iniciando

---
+++ Más/C1/C1.js
@@ -0,0 +1,11 @@
+const users = ["yo", "ella", "el", "ellos"];
+
+const [primera, segunda] = users;
+
+console.log(primera);
+console.log(segunda);
+
+const [tercera] = users[2];
+
+console.log(tercera);
+console.log(users[2]);
